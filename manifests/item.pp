ecb8d68baeb3570e7945495918c0e64703d9c686
Keith Burdis <keith@burdis.org>
2013-01-18 17:02:59 +0000

    When a non-default group is set on a menu or menu item then ensure that the permissions of files and directories are such that only user and group are allowed access.  This prevents access to menus or menu items not to show for users who are not members of the specified group.

--- manifests/item.pp
+++ manifests/item.pp
@@ -7,10 +7,19 @@ define ccfe::item (
 ) {
   $id = $title
   $basedir = $ccfe::basedir
+  case $group {
+    'root': {
+      $mode = '0644'
+    }
+    default: {
+      $mode = '0640'
+    }
+  }
   file { "${basedir}/${parent}.menu/${id}.item":
     ensure  => present,
     owner   => $user,
     group   => $group,
+    mode    => $mode,
     content => template('ccfe/item.erb'),
     require => File["${basedir}/${parent}.menu"],
   }
--- manifests/menu.pp
+++ manifests/menu.pp
@@ -6,20 +6,34 @@ define ccfe::menu (
 ) {
   $id = $title
   $basedir = $ccfe::basedir
+  case $group {
+    'root': {
+      $dir_mode  = '0755'
+      $file_mode = '0644'
+    }
+    default: {
+      $dir_mode  = '0750'
+      $file_mode = '0640'
+    }
+  }
   file { "${basedir}/${id}.menu":
     ensure => directory,
     owner  => $user,
     group  => $group,
+    mode   => $dir_mode,
   }
   file { "${basedir}/${id}.menu/definition":
     ensure  => present,
     owner   => $user,
     group   => $group,
+    mode    => $file_mode,
     content => template('ccfe/definition.erb'),
     require => File["${basedir}/${id}.menu"],
   }
   $defaults = {
     parent => $id,
+    user   => $user,
+    group  => $group,
   }
   create_resources('ccfe::item', $items, $defaults)
 }
